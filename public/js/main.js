0adc0bd5e1a33ab56b4a5b69002294ddd6aa3c99
wellingtongoulart <wellingtongoulart@outlook.com>
2021-12-17 14:36:32 -0300

    organizando

--- public/js/main.js
+++ public/js/main.js
@@ -64,50 +64,6 @@ function inicializaMarcadores() {
   })
 }
 
-function inserePlacar() {
-  var corpoTabela = $('.placar').find('tbody')
-  var usuario = 'Douglas'
-  var numPalavras = $('#contador-palavras').text()
-  var botaoRemover =
-    "<a href='#'><i class='small material-icons'>delete</i></a>"
-
-  var linha = novaLinha(usuario, numPalavras)
-  linha.find('.botao-remover').click(removeLinha)
-
-  corpoTabela.append(linha)
-}
-
-function novaLinha(usuario, palavras) {
-  var linha = $('<tr>')
-  var colunaUsuario = $('<td>').text(usuario)
-  var colunaPalavras = $('<td>').text(palavras)
-  var colunaRemover = $('<td>')
-
-  var link = $('<a>').attr('href', '#').addClass('botao-remover')
-  var icone = $('<i>')
-    .addClass('small')
-    .addClass('material-icons')
-    .text('delete')
-
-  // Icone dentro do <a>
-  link.append(icone)
-
-  // <a> dentro do <td>
-  colunaRemover.append(link)
-
-  // Os três <td> dentro do <tr>
-  linha.append(colunaUsuario)
-  linha.append(colunaPalavras)
-  linha.append(colunaRemover)
-
-  return linha
-}
-
-function removeLinha(event) {
-  event.preventDefault()
-  $(this).parent().parent().remove()
-}
-
 function reiniciaJogo() {
   $('#botao-reiniciar').on('click', function () {
     campo.attr('disabled', false)
